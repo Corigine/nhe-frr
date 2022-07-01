7a61177d2e11815b2dfb57c2ef284301b01ec2a3
huaxingzhu <huaxing.zhu@corigine.com>
2022-07-01 19:51:34 +0800

    NHE-41 modify frr package sh by huaxing.zhu

--- frr_package.sh
+++ frr_package.sh
@@ -75,31 +75,31 @@ chmod 777 dependent-library
 cd ..
 
 mv frr_* frr-* $pkt_name/
-cp frr/frr_install.sh $pkt_name/
-cp frr/third_party/libyang2_2.0.112.1-1/libyang2_2.0.112.1-1_amd64.deb $pkt_name/dependent-library/
-cp frr/third_party/libyang2_2.0.112.1-1/libyang2-dev_2.0.112.1-1_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/frr_install.sh $pkt_name/
+cp nhe-frr/third_party/libyang2_2.0.112.1-1/libyang2_2.0.112.1-1_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/third_party/libyang2_2.0.112.1-1/libyang2-dev_2.0.112.1-1_amd64.deb $pkt_name/dependent-library/
 
-cp frr/dependent-library/gdb_9.2-0ubuntu1_20.04.1_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libbabeltrace1_1.5.8-1build1_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libc6-dev_2.31-0ubuntu9.7_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libc-ares2_1.15.0-1ubuntu0.1_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libc-dev-bin_2.31-0ubuntu9.7_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libcrypt-dev_4.4.10-10ubuntu4_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libdw1_0.176-1.1build1_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libjs-jquery_3.3.1~dfsg-3_all.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libjs-underscore_1.9.1~dfsg-1ubuntu0.20.04.1_all.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libmysqlclient21_8.0.29-0ubuntu0.20.04.3_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libpcre2-16-0_10.34-7_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libpcre2-32-0_10.34-7_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libpcre2-dev_10.34-7_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libpcre2-posix2_10.34-7_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libprotobuf-c1_1.3.3-1_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libsensors5_3.6.0-2ubuntu1.1_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libsensors-config_3.6.0-2ubuntu1.1_all.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libsnmp35_5.8+dfsg-2ubuntu2.3_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/libsnmp-base_5.8+dfsg-2ubuntu2.3_all.deb $pkt_name/dependent-library/
-cp frr/dependent-library/linux-libc-dev_5.4.0-121.137_amd64.deb $pkt_name/dependent-library/
-cp frr/dependent-library/mysql-common_5.8+1.0.5ubuntu2_all.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/gdb_9.2-0ubuntu1_20.04.1_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libbabeltrace1_1.5.8-1build1_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libc6-dev_2.31-0ubuntu9.7_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libc-ares2_1.15.0-1ubuntu0.1_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libc-dev-bin_2.31-0ubuntu9.7_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libcrypt-dev_4.4.10-10ubuntu4_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libdw1_0.176-1.1build1_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libjs-jquery_3.3.1~dfsg-3_all.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libjs-underscore_1.9.1~dfsg-1ubuntu0.20.04.1_all.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libmysqlclient21_8.0.29-0ubuntu0.20.04.3_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libpcre2-16-0_10.34-7_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libpcre2-32-0_10.34-7_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libpcre2-dev_10.34-7_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libpcre2-posix2_10.34-7_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libprotobuf-c1_1.3.3-1_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libsensors5_3.6.0-2ubuntu1.1_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libsensors-config_3.6.0-2ubuntu1.1_all.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libsnmp35_5.8+dfsg-2ubuntu2.3_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/libsnmp-base_5.8+dfsg-2ubuntu2.3_all.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/linux-libc-dev_5.4.0-121.137_amd64.deb $pkt_name/dependent-library/
+cp nhe-frr/dependent-library/mysql-common_5.8+1.0.5ubuntu2_all.deb $pkt_name/dependent-library/
 
 chmod 777 $pkt_name/*
 
